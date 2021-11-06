7fa2aaf599262974f2da22d33a5cb8c101e88201
aliihsanmuhziroglu <aliihsanmuhziroglu@gmail.com>
2021-11-06 14:48:50 +0300

    Added MongoDb Connection

---
+++ DirectoryManager.Core/DirectoryManagerDbConfig.cs
@@ -0,0 +1,11 @@
+﻿ 
+
+namespace DirectoryManager.Core
+{
+    public class DirectoryManagerDbConfig
+    {
+        public string Database_Name { get; set; }
+        public string Directories_Collection_Name { get; set; }
+        public string Connection_String { get; set; }
+    }
+}
