1aad02cc3ae6f9a07826235d5e5f9ed7c4949b41
aliihsanmuhziroglu <aliihsanmuhziroglu@gmail.com>
2021-11-10 06:32:18 +0300

    Added UI Project

---
+++ DirectoryManager.UI/Models/ContactInfoData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectoryManager.UI.Models
+{
+    public class ContactInfoData
+    {
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+    }
+}
